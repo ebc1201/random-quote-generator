2ff8e3f9c7e9045edb424a1f9297273cbd95a681
Erica Chambers <53728223+ebc1201@users.noreply.github.com>
2023-03-27 22:00:38 -0500

    Adjusted quote icons and added button functionality

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const quoteText = document.querySelector('.quote'),
   authorName = document.querySelector('.author .name'),
-  quoteBtn = document.querySelector('button');
+  quoteBtn = document.querySelector('button'),
+  soundBtn = document.querySelector('.sound'),
+  copyBtn = document.querySelector('.copy'),
+  twitterBtn = document.querySelector('.twitter');
 
 const apiKey = '6gtLUdvBF8C1Hu4XMatRHQ==zfROF3XCElZqSTs8';
 
@@ -11,7 +14,8 @@ const options = {
   },
 };
 
-const apiUrl = 'https://api.api-ninjas.com/v1/quotes?limit=1';
+const apiUrl =
+  'https://api.api-ninjas.com/v1/quotes?category=inspirational&limit=1';
 
 async function randomQuote() {
   quoteBtn.classList.add('loading');
@@ -19,11 +23,26 @@ async function randomQuote() {
   const response = await fetch(apiUrl, options);
   const data = await response.json();
 
-  console.log(data);
   quoteText.innerText = data[0].quote;
   authorName.innerText = data[0].author;
   quoteBtn.innerText = 'New Quote';
   quoteBtn.classList.remove('loading');
 }
+
+soundBtn.addEventListener('click', () => {
+  let utterance = new SpeechSynthesisUtterance(
+    `${quoteText.innerText} by ${authorName.innerText}`
+  );
+  speechSynthesis.speak(utterance);
+});
+
+copyBtn.addEventListener('click', () => {
+  navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterBtn.addEventListener('click', () => {
+  let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+  window.open(tweetUrl, '_blank');
+});
 
 quoteBtn.addEventListener('click', randomQuote);
